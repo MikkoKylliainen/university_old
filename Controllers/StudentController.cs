58680d220d99834ea0b05067bc4dafaac1ae6867
Mikko Kylliäinen <mikko.kylliainen@gmail.com>
2022-09-16 09:18:56 +0300

    controller done

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace university
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class StudentController : ControllerBase
+    {
+        [HttpGet()]
+        public string GetAllStudents(){
+            return "This will return all students.";
+        }
+        [HttpGet("{id}")]
+        public string GetOneStudent(int id){
+            return "This will return id "+id;
+        }
+        [HttpPost]
+        public string AddStudent(){
+            return "Add new student";
+        }
+        [HttpPut("{id}")]
+        public string UpdateStudent(int id){
+            return "Update student "+id;
+        }
+        [HttpDelete("{id}")]
+        public string DeleteStudent(int id){
+            return "Delete student "+id;
+        }                     
+    }
+}
